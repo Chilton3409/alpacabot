4cebc00398d5fb5f81f00a0a8a7d7a08d589e858
Nathan Chilton <nate.chilton123@gmail.com>
2025-08-23 12:07:00 -0500

    Add files via upload

--- alpacabot.py
+++ alpacabot.py
@@ -20,10 +20,7 @@ import logging
 import datetime
 from decimal import Decimal
 from meta_ai_api import MetaAI
-import aiofiles
-import uuid
 
-import pyttsx3
 class AlpacaBotLink():
     def __init__(self, api_key, api_secret, paper, access_token):
         #create restclient ref
@@ -34,10 +31,10 @@ class AlpacaBotLink():
         self.api = tradeapi.REST(api_key, api_secret, BASE_URL)
         self.today = datetime.date.today().isoformat()
         self.top_gainers = []
-        my_tickers = {}
+        
         self.ai_api = MetaAI(access_token)
         self.ai_api.access_token = os.environ.get("ACCESS_TOKEN")
-        self.engine = pyttsx3.init()
+        
     async def init(self):
         self.client = TradingClient(api_key=self.api_key, api_secret=self.api_secret)
         self.screener_client= ScreenerClient(api_key=self.api_key, api_secret=self.api_secret)
@@ -49,7 +46,6 @@ class AlpacaBotLink():
         
         except Exception as e:
             logging.exception(msg=e)
-            
     async def get_account(self):
         account = self.api.get_account()
         
@@ -74,54 +70,6 @@ class AlpacaBotLink():
         
         return latest_quote
     
-    async def get_clock(self):
-        try:
-            clock_stuff = self.client.get_clock()
-            return clock_stuff
-        
-        except Exception as e:
-            logging.exception(msg=e)
-            
-    async def add_ticker_to_watchlist(self, id, ticker):
-        try:
-            
-            add_to_watchlist = self.client.add_asset_to_watchlist_by_id(id, ticker)
-            return add_to_watchlist
-        
-        except Exception as e:
-            logging.exception(msg=e)
-            
-    async def get_corpo_announcements(self, ticker):
-        
-        """return a list of corporate announcments"""
-        try:
-            
-            corpo_announcements = self.client.get_corporate_announcements(filter=ticker)
-            return corpo_announcements
-        
-        except Exception as e:
-            logging.exception(msg=e)
-            
-    async def remove_from_watchlist(self, id, ticker):
-        
-        """return a list of corporate announcments"""
-        try:
-            
-            remove_from_watchlist = self.client.remove_asset_from_watchlist_by_id(id, ticker)
-            return remove_from_watchlist
-        except Exception as e:
-            logging.exception(msg=e)
-            
-    async def test(self, ticker):
-        
-        """return a list of corporate announcments"""
-        try:
-            
-            pass
-        
-        except Exception as e:
-            logging.exception(msg=e)
-            
     async def get_bid_price(self, ticker):
         try:
             quote = await self.get_latest_quote(ticker)
@@ -136,15 +84,6 @@ class AlpacaBotLink():
             return quote.ap
         except Exception as e:
             logging.exception(msg=e)
-            
-    async def calculate_spread(self,bid_price, ask_price):
-        try:
-            spread = Decimal(bid_price) - Decimal(ask_price)
-            
-            return spread
-        
-        except Exception as e:
-            logging.exception(msg=e)
     
     async def get_asset(self, ticker: str):
         """
@@ -156,33 +95,43 @@ class AlpacaBotLink():
         except Exception as e:
             logging.error(f"an error occured in the get asset function: {e}")
             
-    
-    async def get_news(self, ticker, limit):
+    async def get_last_price(self, ticker):
+        try:
+            pass
+        
+        except Exception as e:
+            logging.exception(msg=e)
+            
+    async def get_news(self, ticker):
         
         """
         retrieve the news for a single ticker
         """
         try:
-            news = self.api.get_news(symbol=ticker, limit=limit, include_content=True, exclude_contentless=True)
+            news = self.api.get_news(symbol=ticker, limit=1, include_content=True, exclude_contentless=True)
             if news:
                 
                 for content in news:
-                    if content.content:
-                        return content.content
-                        
+                    if content:
+                        #use meta ai to analyze the content
+                        information = content.content
+                        await asyncio.sleep(5.0)    
+                        sentiment_analysis = await self.get_meta_ai_insights(f"analyze this news article to generate buy and sell signals for ticker: {ticker}: {information}. Return Buy or Pass")
+                        await asyncio.sleep(5.0)                 
+                        return sentiment_analysis
                     else:
                         return logging.info(f"no news available for: {ticker}")
             
         except Exception as e:
             logging.exception(msg=e)
+    
             
     async def get_market_movers(self):
         """
         returns the latest marker movers
         """
         try:
-            market_movers = self.screener_client.get_market_movers(request_params=MarketMoversRequest(top=50))
-            
+            market_movers = self.screener_client.get_market_movers(request_params=MarketMoversRequest(top=30))
             return market_movers.gainers
 
         except Exception as e:
@@ -190,40 +139,15 @@ class AlpacaBotLink():
     async def create_top_gainer_list(self):
         try:
             #extract
-            market_movers = await self.get_market_movers()
+            test = await self.get_market_movers()
             
-            for mover in market_movers:
-                if mover.price > Decimal('1.00'):
-                    
-                    self.top_gainers.append(mover.symbol)
+            for t in test:
                 
+                self.top_gainers.append(t.symbol)
+            
             return
         except Exception as e:
             logging.exception(msg=e)
-            
-    async def create_watchlist(self):
-        """
-        create a new watchlist
-        """
-        try:
-            watchlist = self.api.create_watchlist(watchlist_name='meta_watchlist')
-            return watchlist
-        
-        except Exception as e:
-            logging.exception(msg=e)
-            
-    async def get_watchlist(self):
-        """
-        get watchlist and return watchlist id
-        """
-        try:
-            watchlists = self.api.get_watchlists(watchlist_name='meta_watchlist')
-            return watchlists
-        
-        except Exception as e:
-            logging.exception(msg=e)
-            
-    
     async def get_most_active():
         pass
     
@@ -244,21 +168,10 @@ class AlpacaBotLink():
             logging.exception(msg=e)
             
     async def get_trades(self):
-        """
-        get a history of the most recent trades
-        """
         pass
     
     async def get_snapshot(self, ticker):
-        """
-        get a snapshot of the current ticker, whatever that means
-        """
-        try:
-            snapshot = self.api.get_snapshot(ticker)
-            return snapshot
-        
-        except Exception as e:
-            logging.exception(msg=e)
+        pass
     
     
             
@@ -274,83 +187,56 @@ class AlpacaBotLink():
         except Exception as e:
             logging.error(msg=e)
             
-    async def buy_logic(self ):
+    async def buy_logic(self):
         #first we need to gather the the top gainers
-        
         #pass the top gainers tickers into the top gainers list
-        
-        #get the barset for each ticker and pass it to the machine learning model
-        #when buy signals are generated, create a limit buy order
-        #then we need to do the same thing for crypto
-        #gather the crypto top gainers
-        #store them in the crypto top gainers list
-        #get the barset for each ticker and pass it to the machine learning model
-        #to generate buy signals
-        #then create a buy order when buy signals are generated
-        #then use meta to evaluate the portfolio for potential optimizartions 
-        #acrross different asset classes
-        
-        
+        await self.create_top_gainer_list()
     
-        pass
+        #get the barset for each ticker and pass it to the machine learning model
+        for ticker in self.top_gainers:
+            candles = await self.get_candles(ticker=ticker)
+            print(candles)
+            news = await self.get_news(ticker=ticker)
+            print(news)
+            #when buy signals are generated, create a limit buy order
+            
+            #then we need to do the same thing for crypto
+            #create a crypto buy_logic cycle
+            
+            #gather the crypto top gainers
+            #store them in the crypto top gainers list
+            #get the barset for each ticker and pass it to the machine learning model
+            #to generate buy signals
+            #then create a buy order when buy signals are generated
+            #then use meta to evaluate the portfolio for potential optimizartions 
+            #acrross different asset classes
+            
+        
+        
+      
+        
+        return
 
     async def get_candles(self, ticker):
         # Retrieve 1-minute bar data
        
-        try:
-            
-            bars = self.api.get_latest_bars(ticker)
-            return bars
-        except Exception as e:
-            logging.exception(msg=e)
-    #put all of the self.api function calls here
-    async def get_latest_crypto_bars(self, ticker):
-        try:
-            latest_bars = self.api.get_latest_crypto_bars(ticker)
-            return latest_bars
-        
-        except Exception as e:
-            logging.exception(msg=e)
-    async def list_assets(self, ticker):
-        try:
-            assets = self.api.list_assets()
-            return assets
-        
-        except Exception as e:
-            logging.exception(msg=e)
-    
-    
-    async def test(self, ticker):
-        try:
-            pass
-        
-        except Exception as e:
-            logging.exception(msg=e)
+
+        test = self.api.get_latest_bars(ticker)
+        return test
         
     
-    #begin meta ai functions 
-    async def optimize_portfolio(self, my_tickers:dict):
-        """
-        use meta ai api to analyze portfolio unrealized gaines and losses
-        for potential dollar cost averaging oppurtunities, and to maximize potential
-        gains within the my tickers dict{symbol:pnl}
-        
-        """
+    
+    async def optimize_portfolio(self):
         pass
     
     
     async def sell_logic():
         
         pass
-    async def create_limit_buy_order(self, ticker):
+    async def create_order(self):
         try:
-            time_in_force = 'day'
-            limit_price = await self.get_bid_price(ticker)
-            qty = await self.trade_amount(ticker=ticker)
-            
-            order_id = uuid.uuid4()
-            
             pass
+        
         except Exception as e:
             logging.exception(msg=e)
             
@@ -374,10 +260,10 @@ class AlpacaBotLink():
     async def submit_buy_limit_order(self, ticker: str, qty: float, limit_price: float):
         order_data = LimitOrderRequest(
             symbol=ticker,
-            qty=await self.trade_amount(),
+            qty=qty,
             side=OrderSide.BUY,
             time_in_force=TimeInForce.DAY,
-            limit_price=await self.get_bid_price(ticker)
+            limit_price=limit_price
         )
     
         try:
@@ -402,7 +288,6 @@ class AlpacaBotLink():
         except Exception as e:
             logging.exception(msg=e)
             return None
-        
     async def close_position(self, ticker):
         try:
             test = self.client.close_position(symbol_or_asset_id=ticker,close_options=[1, 100])
@@ -427,77 +312,20 @@ class AlpacaBotLink():
             return response['message']
         except Exception as e:
             logging.exception(msg=e)
-            return logging.info("theres been a problem getting a response from meta")
-
-    async def syn_voice(self, text:str):
-        try:
-            
-            if self.engine:
-                self.engine.say(text)
-                self.engine.runAndWait()
-                return
-         
-        except Exception as e:
-            logging.exception(msg=e)
+            return None
         
-        
-            
     
-    async def read_file(self, filename:str) -> str:
-        """
-        read the contents of a file
-        """
-        try:
-            async with aiofiles.open(filename, 'r') as f:
-                await f.read()
-                return
-            
-        except Exception as e:
-            logging.exception(msg=e)
     
-    async def append_file(self, filename:str, text: str):
-        try:
-            async with aiofiles.open(filename, 'a') as f:
-                await f.write("\n")
-                await f.write(str(text))
-        
-        except Exception as e:
-            logging.exception(msg=e)
-        
-    async def write_file(self, filename: str, text: str) -> str:
-        try:
-            async with aiofiles.open(filename, 'w') as f:
-                await f.write(str(text))
-                logging.info(f"writing to file: {filename}")
-            return
-                
-        
-        except Exception as e:
-            logging.exception(msg=e)
-            
-    async def get_user_input(self):
-        user_input = input("Please enter a ticker you want to analyze: ")
-        return user_input
-    
-    async def create_filename(self):
-        try:
-            task = input("create a name for the file: \n")
-            return task
-        except Exception as e:
-            logging.exception(msg=e)
 async def main():
     access_token = os.environ.get("META_AI_TOKEN")
     api_key = os.environ.get("API_KEY")
     api_secret = os.environ.get("API_SECRET")
     client = AlpacaBotLink(api_key, api_secret, paper=True, access_token=access_token)
+    #add the below code to the buy logic cycle
+    await client.buy_logic()
     
-    await client.create_top_gainer_list()
-    for ticker in client.top_gainers:
-        candles = await client.get_candles(ticker)
-        print(candles)
-        
-        await asyncio.sleep(5.00)
-       
+    
+   
     return
 
 if __name__=='__main__':
